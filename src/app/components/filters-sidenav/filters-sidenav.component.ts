44c7613495af2731e5a4fdacb3ee8eb3cb747cd4
Jorge Vilariño <jorgevilarino05@gmail.com>
2025-01-08 00:37:40 +0100

    Added new components and functionality to the filters sidenav and new models

--- src/app/components/filters-sidenav/filters-sidenav.component.ts
+++ src/app/components/filters-sidenav/filters-sidenav.component.ts
@@ -1,15 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatExpansionModule } from '@angular/material/expansion'
 import { MatSliderModule } from '@angular/material/slider';
+import { Category } from '../../models/category.model';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment.development';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Brand } from '../../models/brand.model';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-filters-sidenav',
-  imports: [ MatIconModule, MatButtonModule, MatExpansionModule, MatSliderModule ],
+  imports: [ MatIconModule, MatButtonModule, MatExpansionModule, MatSliderModule, CurrencyPipe ],
   templateUrl: './filters-sidenav.component.html',
   styleUrl: './filters-sidenav.component.css'
 })
 export class FiltersSidenavComponent {
+  httpClient = inject(HttpClient);
+  
+  minPriceValue = 0;
+  maxPriceValue = 1500;
+  categories: Category[] = [];
+  brands: Brand[] = [];
 
+  constructor() {
+    this.getCategories();
+  }
+
+  public selectCategory(id: number): void {
+    this.changeBrands(this.categories.find(x => x.id == id)!);
+  }
+
+  private getCategories(): void {
+    this.httpClient.get<any>(
+      `${environment.apiBaseUrl}categories`, 
+      { observe: 'response' })
+      .pipe(takeUntilDestroyed())
+      .subscribe(result => {
+        if(result.ok) {
+          this.categories = result.body!.categories;
+        }
+      }
+    );
+  }
+
+  private changeBrands(category: Category): void {
+    this.httpClient.get<any>(
+      `${environment.apiBaseUrl}brands/${category.id}`, 
+      { observe: 'response' })
+      .subscribe(result => {
+        if(result.ok) {
+          this.brands = result.body!.brands;
+        }
+      }
+    );
+  }
 }
