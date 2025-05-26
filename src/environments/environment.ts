c1c7b946477b3429749991317dc6de7ec43232c8
Jorge Vilariño <jorgevilarino05@gmail.com>
2025-05-26 21:07:47 +0200

    Revert last changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiBaseUrl: 'https://backend.byteshop-t.com/api/',
-  apiImageUrl: 'https://backend.byteshop-t.com/images/'
+  apiBaseUrl: 'https://backend.byteshop.com/api/',
+  apiImageUrl: 'https://backend.byteshop.com/images/'
 };
