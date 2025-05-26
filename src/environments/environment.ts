129a4516f64134e7626aca07d0a60a8f21f7c6bb
Jorge Vilariño <jorgevilarino05@gmail.com>
2025-05-26 21:19:28 +0200

    Change backend domain name because it is used in the web.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiBaseUrl: 'https://backend.byteshop.com/api/',
-  apiImageUrl: 'https://backend.byteshop.com/images/'
+  apiBaseUrl: 'https://backend.byteshop.gg/api/',
+  apiImageUrl: 'https://backend.byteshop.gg/images/'
 };
